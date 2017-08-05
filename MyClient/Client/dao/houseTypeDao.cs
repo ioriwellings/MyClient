7ec9da9d517112be13393b1d86f29ca7e05655bf
zuyunfei <2880985431@qq.com>
2017-08-05 13:17:51 +0800

    zuyunfei

--- MyClient/Client/dao/houseTypeDao.cs
+++ MyClient/Client/dao/houseTypeDao.cs
@@ -6,165 +6,64 @@ using System.Threading.Tasks;
 using System.Data;
 using System.Data.SQLite;
 using System.Threading;
+using BOYA.CRMS.Common;
+using Common;
 
 namespace LBKJClient.dao
 {
     class houseTypeDao
     {
         private static readonly object obj = new object();
-        private string dbPath = "Data Source =new.baw";
         public DataTable queryhouseType()
         {
             DataSet ds = new DataSet();
             string sql = "select * from lb_house_type";
-            SQLiteConnection conn = null;
-            conn = new SQLiteConnection(dbPath);
-            SQLiteDataAdapter comm = null;
-            conn.SetPassword(frmLogin.dataPassw);
-            try
-            {
-                if (conn.State != ConnectionState.Open)
-                {
-                    conn.Open();
-                }
-                comm = new SQLiteDataAdapter(sql, conn);
-                comm.Fill(ds);
-            }
-            catch (System.Data.SQLite.SQLiteException ex)
-            {
-                throw new Exception(ex.Message);
-            }
-            finally
-            {
-                comm.Dispose();//释放命令执行器
-                conn.Close();//断开数据库连接
-                conn.Dispose();//释放连接器
-            }
+
+            ds = DbHelperMySQL.Query(sql);
             return ds.Tables[0];
         }
         public bool updateHouseTypeById(string imgpath,string id)
         {
             int ret = 0;
             String sql = "update lb_house_type set imgPath='" + imgpath + "'where id = '" + id + "'";
-            SQLiteConnection conn = null;
-            conn = new SQLiteConnection(dbPath);
-            conn.SetPassword(frmLogin.dataPassw);
-            if (conn.State != ConnectionState.Open)
-            {
-                conn.Open();
-            }
-            SQLiteCommand cmdSearch = null;
-            try
-            {
-                cmdSearch = new SQLiteCommand(sql, conn);
                 Monitor.Enter(obj);
-                ret = cmdSearch.ExecuteNonQuery();
-                Monitor.Exit(obj);
-            }
-            catch (SQLiteException ex)
-            {
-                throw new Exception(ex.Message);
-            }
-            finally
-            {
-                cmdSearch.Dispose();//释放命令执行器
-                conn.Close();//断开数据库连接
-                conn.Dispose();//释放连接器
-            }
+                ret = DbHelperMySQL.ExecuteSql(sql);
+            Monitor.Exit(obj);
+
             return ret == 0 ? false : true;
         }
         public bool deleteHouseTypeById(string id)
         {
             int ret = 0;
             String sql = "delete from lb_house_type where id = '" + id + "'";
-            SQLiteConnection conn = null;
-            conn = new SQLiteConnection(dbPath);
-            conn.SetPassword(frmLogin.dataPassw);
-            if (conn.State != ConnectionState.Open)
-            {
-                conn.Open();
-            }
-            SQLiteCommand cmdSearch = null;
-            try
-            {
-                cmdSearch = new SQLiteCommand(sql, conn);
+
                 Monitor.Enter(obj);
-                ret = cmdSearch.ExecuteNonQuery();
-                Monitor.Exit(obj);
-            }
-            catch (SQLiteException ex)
-            {
-                throw new Exception(ex.Message);
-            }
-            finally
-            {
-                cmdSearch.Dispose();//释放命令执行器
-                conn.Close();//断开数据库连接
-                conn.Dispose();//释放连接器
-            }
+                ret = DbHelperMySQL.ExecuteSql(sql);
+            Monitor.Exit(obj);
+
             return ret == 0 ? false : true;
         }
 
         public bool addHouseManage(bean.houseInfo hi)
         { 
             int ret = 0;
-            String sql = "insert into lb_house_type (name,t_high,t_low,h_high,h_low) values ('" + hi.name + "', '" + hi.t_high + "', '" + hi.t_low + "', '" + hi.h_high + "', '" + hi.h_low + "')";
-            SQLiteConnection conn = null;
-            conn = new SQLiteConnection(dbPath);
-            SQLiteCommand cmdSearch = null;
-            conn.SetPassword(frmLogin.dataPassw);
-            if (conn.State != ConnectionState.Open)
-            {
-                conn.Open();
-            }
-            try
-            {
-                cmdSearch = new SQLiteCommand(sql, conn);
-                Monitor.Enter(obj);
-                ret = cmdSearch.ExecuteNonQuery();
-                Monitor.Exit(obj);
-            }
-            catch (SQLiteException ex)
-            {
-                throw new Exception(ex.Message);
-            }
-            finally
-            {
-                cmdSearch.Dispose();//释放命令执行器
-                conn.Close();//断开数据库连接
-                conn.Dispose();//释放连接器
-            }
+            string id = Result.GetNewId();
+            String sql = "insert into lb_house_type (id,name,t_high,t_low,h_high,h_low) values ('" + id + "','" + hi.name + "', '" + hi.t_high + "', '" + hi.t_low + "', '" + hi.h_high + "', '" + hi.h_low + "')";
+
+            Monitor.Enter(obj);
+            ret = DbHelperMySQL.ExecuteSql(sql);
+            Monitor.Exit(obj);
+
             return ret == 0 ? false : true;
         }
         public bool updateHouseInfoById(bean.houseInfo hi)
         {
             int ret = 0;
             String sql = "update lb_house_type set name='" + hi.name + "', t_high='" + hi.t_high + "', t_low='" + hi.t_low + "', h_high='" + hi.h_high + "', h_low='" + hi.h_low + "',isUsed='"+hi.isUsed+"' where id = '" + hi.id + "'";
-            SQLiteConnection conn = null;
-            conn = new SQLiteConnection(dbPath);
-            conn.SetPassword(frmLogin.dataPassw);
-            if (conn.State != ConnectionState.Open)
-            {
-                conn.Open();
-            }
-            SQLiteCommand cmdSearch = null;
-            try
-            {
-                cmdSearch = new SQLiteCommand(sql, conn);
-                Monitor.Enter(obj);
-                ret = cmdSearch.ExecuteNonQuery();
-                Monitor.Exit(obj);
-            }
-            catch (SQLiteException ex)
-            {
-                throw new Exception(ex.Message);
-            }
-            finally
-            {
-                cmdSearch.Dispose();//释放命令执行器
-                conn.Close();//断开数据库连接
-                conn.Dispose();//释放连接器
-            }
+
+            Monitor.Enter(obj);
+            ret = DbHelperMySQL.ExecuteSql(sql);
+            Monitor.Exit(obj);
             return ret == 0 ? false : true;
         }
     }
--- MyClient/Client/dao/manageHostDao.cs
+++ MyClient/Client/dao/manageHostDao.cs
@@ -6,6 +6,8 @@ using System.Threading;
 using System.Data;
 using System.Data.SQLite;
 using System.Windows;
+using BOYA.CRMS.Common;
+using Common;
 
 namespace LBKJClient.dao
 {
@@ -16,33 +18,12 @@ namespace LBKJClient.dao
         public bool addmanageHostDao(bean.manageHose mh)
         {
             string time = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            string id = Result.GetNewId();
             int ret = 0;
-            String sql = "insert into lb_managehost_info (measureCode,hostName,hostAddress,CommunicationType,serialPort,portNumber,storeType,createTime,houseType) values ('" + mh.measureCode + "', '" + mh.hostName + "', '" + mh.hostAddress + "', '" + mh.CommunicationType + "', '" + mh.serialPort + "', '" + mh.portNumber + "', '" + mh.storeType + "', '" + time + "', '" + mh.houseType + "')";
-            SQLiteConnection conn = null;
-            conn = new SQLiteConnection(dbPath);
-            SQLiteCommand cmdSearch = null;
-            conn.SetPassword(frmLogin.dataPassw);
-            try
-            {
-                if (conn.State != ConnectionState.Open)
-            {
-                conn.Open();
-            }
-                cmdSearch = new SQLiteCommand(conn);
-                cmdSearch.CommandText = sql;
-                Monitor.Enter(obj);
-                ret = cmdSearch.ExecuteNonQuery();
-                Monitor.Exit(obj);
-            }
-            catch(SQLiteException ex)
-            {
-                throw new Exception(ex.Message);
-            }
-            finally {
-            cmdSearch.Dispose();//释放命令执行器
-            conn.Close();//断开数据库连接
-            conn.Dispose();//释放连接器
-            }
+            String sql = "insert into lb_managehost_info (id,measureCode,hostName,hostAddress,CommunicationType,serialPort,portNumber,storeType,createTime,houseType) values ('" + id + "','" + mh.measureCode + "', '" + mh.hostName + "', '" + mh.hostAddress + "', '" + mh.CommunicationType + "', '" + mh.serialPort + "', '" + mh.portNumber + "', '" + mh.storeType + "', '" + time + "', '" + mh.houseType + "')";
+            Monitor.Enter(obj);
+            ret = DbHelperMySQL.ExecuteSql(sql);
+            Monitor.Exit(obj);
             return ret == 0 ? false : true;
         }
 
@@ -50,150 +31,50 @@ namespace LBKJClient.dao
             String sql = "SELECT m.*,h.name FROM lb_managehost_info m join lb_house_type h on m.houseType=h.id";
             DataSet ds = new DataSet();
             ds.Clear();
-            SQLiteConnection conn = null;
-            conn = new SQLiteConnection(dbPath);
-            SQLiteDataAdapter comm = null;
-            conn.SetPassword(frmLogin.dataPassw);
-            try
-            {
-                if (conn.State != ConnectionState.Open)
-            {
-                conn.Open();
-            }
-                comm = new SQLiteDataAdapter(sql, conn);
-                comm.Fill(ds);
-            }
-            catch (SQLiteException ex)
-            {
-                throw new Exception(ex.Message);
-            }
-            finally
-            {
-                comm.Dispose();//释放命令执行器
-                conn.Close();//断开数据库连接
-                conn.Dispose();//释放连接器
-            }
+
+            ds = DbHelperMySQL.Query(sql);
             return ds.Tables[0];
         }
         public bool deletemanageHostDao(string id,string code) {
             int ret = 0;
             int cd = 0;
             String sql = "delete from lb_managehost_info where id = '"+id+"'";
-            SQLiteConnection conn = null;
-            conn = new SQLiteConnection(dbPath);
-            SQLiteCommand cmdSearch = null;
-            conn.SetPassword(frmLogin.dataPassw);
-            try
-            {
-                if (conn.State != ConnectionState.Open)
-            {
-                conn.Open();
-            }
-                cmdSearch = new SQLiteCommand(sql,conn);
                 Monitor.Enter(obj);
-                ret = cmdSearch.ExecuteNonQuery();
+                ret = DbHelperMySQL.ExecuteSql(sql);
                 if (ret>0) {
-                    cmdSearch.Dispose();//释放命令执行器
                     string sql1 = "delete from lb_device_information where measureCode = '"+code+"'";
-                    cmdSearch = new SQLiteCommand(sql1, conn);
                     Monitor.Enter(obj);
-                    cd =cmdSearch.ExecuteNonQuery();  
+                    cd = DbHelperMySQL.ExecuteSql(sql1);
                 }
                 Monitor.Exit(obj);
-            }
-            catch (SQLiteException ex)
-            {
-                throw new Exception(ex.Message);
-            }
-            finally { 
-            cmdSearch.Dispose();//释放命令执行器
-            conn.Close();//断开数据库连接
-            conn.Dispose();//释放连接器
-            }
+
             return cd == 0 ? false : true;
         }
         public bool updatemanageHostDao(bean.manageHose mh)
         {
             int ret = 0;
-            int cd = 0;
             String sql = "update lb_managehost_info set hostName='"+mh.hostName+"',hostAddress='"+mh.hostAddress+ "',CommunicationType='"+mh.CommunicationType+ "',serialPort='"+mh.serialPort+ "',portNumber='"+mh.portNumber+ "',storeType='"+mh.storeType+ "',houseType='" + mh.houseType + "' where measureCode = '" + mh.measureCode+"'";
-            SQLiteConnection conn = null;
-            conn = new SQLiteConnection(dbPath);
-            SQLiteCommand cmdSearch = null;
-            conn.SetPassword(frmLogin.dataPassw);
-            try
-            {
-                if (conn.State != ConnectionState.Open)
-            {
-                conn.Open();
-            }
-                cmdSearch = new SQLiteCommand(sql, conn);
-                Monitor.Enter(obj);
-                ret = cmdSearch.ExecuteNonQuery();
-                //if (ret > 0)
-                //{
-                //    cmdSearch.Dispose();//释放命令执行器
-                //    string sql1 = "delete from lb_device_information where measureCode = '" + mh.measureCode + "'";
-                //    cmdSearch = new SQLiteCommand(sql1, conn);
-                //    cd = cmdSearch.ExecuteNonQuery();
-                //}
-                Monitor.Exit(obj);
-            }
-            catch (SQLiteException ex)
-            {
-                throw new Exception(ex.Message);
-            }
-            finally
-            {
-                cmdSearch.Dispose();//释放命令执行器
-                conn.Close();//断开数据库连接
-                conn.Dispose();//释放连接器
-            }
-            return cd == 0 ? false : true;
+            Monitor.Enter(obj);
+            ret = DbHelperMySQL.ExecuteSql(sql);
+            Monitor.Exit(obj);
+
+            return ret == 0 ? false : true;
         }
         public bool updateManageHostCdNum(bean.manageHose mh)
         {
             int ret = 0;
             int cd = 0;
             String sql = "SELECT count(1) FROM lb_device_information where measureCode='" + mh.measureCode + "'";
-            SQLiteConnection conn = null;
-            conn = new SQLiteConnection(dbPath);
-            SQLiteCommand cmdSearch = null;
-            conn.SetPassword(frmLogin.dataPassw);
-            try
-            {
-                if (conn.State != ConnectionState.Open)
-                {
-                    conn.Open();
-                }
-                cmdSearch = new SQLiteCommand(conn);
-                cmdSearch.CommandText = sql;
-                SQLiteDataReader reader = cmdSearch.ExecuteReader();
+            ret = DbHelperMySQL.ExecuteSql(sql);
 
-                while (reader.Read())
-                {
-                    ret = reader.GetInt16(0);
-                }
-                Monitor.Enter(obj);
+            Monitor.Enter(obj);
                 if (ret > 0)
                 {
-                    cmdSearch.Dispose();//释放命令执行器
                     string sql1 = "update lb_managehost_info set portNumber='" + ret.ToString() + "' where measureCode = '" + mh.measureCode + "'";
-                    cmdSearch = new SQLiteCommand(sql1, conn);
-                    cd = cmdSearch.ExecuteNonQuery();
+                    cd = DbHelperMySQL.ExecuteSql(sql1);
                 }
                 Monitor.Exit(obj);
-            }
-            catch (SQLiteException ex)
-            {
-                throw new Exception(ex.Message);
-            }
-            finally
-            {
-                cmdSearch.Dispose();//释放命令执行器
-                conn.Close();//断开数据库连接
-                conn.Dispose();//释放连接器
-            }
+
             return cd == 0 ? false : true;
         }
         //查询管理主机中  使用串口通信的主机信息  判断地址栏不为空
@@ -202,29 +83,7 @@ namespace LBKJClient.dao
             String sql = "select * from lb_managehost_info where hostAddress > 0 and CommunicationType='[管理主机]LB863RSB_N1(LBGZ-02)' order by hostAddress";
             DataSet ds = new DataSet();
             ds.Clear();
-            SQLiteConnection conn = null;
-            conn = new SQLiteConnection(dbPath);
-            SQLiteDataAdapter comm = null;
-            conn.SetPassword(frmLogin.dataPassw);
-            try
-            {
-                if (conn.State != ConnectionState.Open)
-            {
-                conn.Open();
-            }
-                comm = new SQLiteDataAdapter(sql, conn);
-                comm.Fill(ds);
-            }
-            catch (SQLiteException ex)
-            {
-                throw new Exception(ex.Message);
-            }
-            finally
-            {
-                comm.Dispose();//释放命令执行器
-                conn.Close();//断开数据库连接
-                conn.Dispose();//释放连接器
-            }
+            ds = DbHelperMySQL.Query(sql);
             return ds.Tables[0];
         }
         public DataTable queryManageHoststoreType(string measureCode)
@@ -232,29 +91,7 @@ namespace LBKJClient.dao
             String sql = "select id,measureCode,storeType from lb_managehost_info where measureCode = '" + measureCode + "'";
             DataSet ds = new DataSet();
             ds.Clear();
-            SQLiteConnection conn = null;
-            conn = new SQLiteConnection(dbPath);
-            SQLiteDataAdapter comm = null;
-            conn.SetPassword(frmLogin.dataPassw);
-            try
-            {
-                if (conn.State != ConnectionState.Open)
-                {
-                    conn.Open();
-                }
-                comm = new SQLiteDataAdapter(sql, conn);
-                comm.Fill(ds);
-            }
-            catch (SQLiteException ex)
-            {
-                throw new Exception(ex.Message);
-            }
-            finally
-            {
-                comm.Dispose();//释放命令执行器
-                conn.Close();//断开数据库连接
-                conn.Dispose();//释放连接器
-            }
+            ds = DbHelperMySQL.Query(sql);
             return ds.Tables[0];
         }
     }
