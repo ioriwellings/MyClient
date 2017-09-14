c32615363e8642105a8f989f35dd1324305bdebc
a <a@MAHAIFENG-PC>
2017-09-14 15:59:47 +0800

    软件传输显示数据实时变动，杜绝硬件数据不传输以后软件长时间不更新，导致对数据情况无法实时掌握，        这个把30调为10.

--- MyClient/Client/Properties/Settings.Designer.cs
+++ MyClient/Client/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace LBKJClient.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("30")]
+        [global::System.Configuration.DefaultSettingValueAttribute("10")]
         public string overTime {
             get {
                 return ((string)(this["overTime"]));
